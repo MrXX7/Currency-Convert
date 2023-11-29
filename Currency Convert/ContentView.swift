30d831a0c39fe416843f0f7b0b80b8d5b18797b1
Oncu <oohancan@gmail.com>
2023-11-29 03:32:28 +0300

    Exchange rates are updated with more realistic values. Add the custom rate entered by the user manually. ConvertedAmount property calculates the total amount based on user input and selected exchange rate. Display the total amount with the appropriate currency.

--- Currency Convert/ContentView.swift
+++ Currency Convert/ContentView.swift
@@ -7,18 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CurrencyConvertView: View {
+    @State private var euroAmount = ""
+    @State private var selectedCurrencyIndex = 0
+    @State private var customExchangeRate = ""
+
+    let currencies = ["USD", "EUR", "GBP", "JPY", "TRY"]
+
+    var exchangeRates: [Double] {
+        var rates = [1.108, 1.0, 0.85, 135, 31.81]  //
+        
+        if let customRate = Double(customExchangeRate) {
+            rates[selectedCurrencyIndex] = customRate
+        }
+
+        return rates
+    }
+
+    var convertedAmount: Double {
+        let euroValue = Double(euroAmount) ?? 0
+        let selectedRate = exchangeRates[selectedCurrencyIndex]
+        return euroValue * selectedRate
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter Euro Amount", text: $euroAmount)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.decimalPad)
+
+            Picker("Currency", selection: $selectedCurrencyIndex) {
+                ForEach(0..<currencies.count) {
+                    Text(self.currencies[$0])
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            HStack {
+                Text("Exchange Rate:")
+                TextField("Enter Custom Rate", text: $customExchangeRate)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+            }
+            .padding(.vertical, 8)
+
+            Text("Total Amount: \(convertedAmount, specifier: "%.2f") \(currencies[selectedCurrencyIndex])")
+                .padding()
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct CurrencyConvertView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyConvertView()
+    }
 }
--- Currency Convert/Currency_ConvertApp.swift
+++ Currency Convert/Currency_ConvertApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Currency_ConvertApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CurrencyConvertView()
         }
     }
 }
