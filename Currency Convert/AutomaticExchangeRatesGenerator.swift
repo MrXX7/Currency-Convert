d89386252d7b4b238ade1d95e97157cce50c0c84
Oncu <oohancan@gmail.com>
2024-05-24 13:00:59 +0300

    Fixed issue where TextField was obscured by keyboard

--- Currency Convert/AutomaticExchangeRatesGenerator.swift
+++ Currency Convert/AutomaticExchangeRatesGenerator.swift
@@ -5,32 +5,6 @@
 //  Created by Oncu Can on 6.12.2023.
 //
 
-//import Foundation
-//
-//class AutomaticExchangeRatesGenerator {
-//    static func generateRates(currencies: [String], exchangeRates: [String: Double], flags: [String: String]) -> [String] {
-//        let automaticExchangeRates: [String] = currencies.compactMap { currency in
-//            if currency.localizedCaseInsensitiveCompare("EUR") == .orderedSame {
-//                return nil
-//            }
-//
-//            let rate = exchangeRates[currency] ?? 0.0
-//
-//            let formatter = NumberFormatter()
-//            formatter.minimumFractionDigits = 2
-//            formatter.maximumFractionDigits = 4
-//            formatter.numberStyle = .decimal
-//
-//            guard let formattedRate = formatter.string(from: NSNumber(value: rate)) else {
-//                print("Formatting Error for \(currency)")
-//                return "1 \(flags[currency] ?? "") = \(rate) \(currency)"
-//            }
-//            return "\(flags[currency] ?? "") 1 EUR = \(formattedRate) \(currency)"
-//        }
-//
-//        return automaticExchangeRates
-//    }
-//}
 import Foundation
 
 class AutomaticExchangeRatesGenerator {
--- Currency Convert/CurrencyConvertView.swift
+++ Currency Convert/CurrencyConvertView.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 import Alamofire
+import Combine
+
+final class KeyboardResponder: ObservableObject {
+    @Published var currentHeight: CGFloat = 0
+    private var cancellableSet: Set<AnyCancellable> = []
+
+    init() {
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .merge(with: NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification))
+            .compactMap { notification -> CGFloat? in
+                if notification.name == UIResponder.keyboardWillShowNotification {
+                    return (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height
+                } else {
+                    return 0
+                }
+            }
+            .assign(to: \.currentHeight, on: self)
+            .store(in: &cancellableSet)
+    }
+}
 
 struct CurrencyConvertView: View {
     @State private var euroAmount = ""
@@ -15,43 +35,49 @@ struct CurrencyConvertView: View {
     @Binding var isDarkMode: Bool
     @State private var exchangeRates: [String: Double] = [:]
     @State private var selectedRateCurrencyIndex = 0
+    @StateObject private var keyboard = KeyboardResponder()
 
     let currencies = ["EUR", "USD", "GBP", "TRY", "CAD", "CHF", "SAR", "AUD", "CNY"]
-    let flags: [String: String] = ["EUR": "🇪🇺","USD": "🇺🇸", "GBP": "🇬🇧", "TRY": "🇹🇷", "CAD": "🇨🇦", "CHF": "🇨🇭", "SAR": "🇸🇦", "AUD": "🇦🇺", "CNY": "🇨🇳"]
+    let flags: [String: String] = ["EUR": "🇪🇺", "USD": "🇺🇸", "GBP": "🇬🇧", "TRY": "🇹🇷", "CAD": "🇨🇦", "CHF": "🇨🇭", "SAR": "🇸🇦", "AUD": "🇦🇺", "CNY": "🇨🇳"]
 
     var body: some View {
         let convertedAmount = AmountConverter.convertAmount(euroAmount: euroAmount, customExchangeRate: customExchangeRate, selectedCurrencyIndex: selectedCurrencyIndex, exchangeRates: exchangeRates, currencies: currencies)
         
         let automaticExchangeRates = AutomaticExchangeRatesGenerator.generateRates(currencies: currencies, exchangeRates: exchangeRates, flags: flags, baseCurrency: currencies[selectedRateCurrencyIndex])
         
-        return VStack {
-            HStack {
-                Spacer()
-                DarkModeToggleButton(isDarkMode: $isDarkMode)
-            }
-            .preferredColorScheme(isDarkMode ? .dark : .light)
-            
-            AmountInputView(euroAmount: $euroAmount)
-            CurrencyPickerView(currencies: currencies, selectedCurrencyIndex: $selectedCurrencyIndex)
-            //            if selectedCurrencyIndex != 0 { // Show exchange rate input if not EUR
-            ExchangeRateInputView(customExchangeRate: $customExchangeRate)
-            //            }
-            HStack {
-                TotalAmountView(convertedAmount: convertedAmount, selectedCurrency: currencies[selectedCurrencyIndex])
-                ResetButton(euroAmount: $euroAmount, customExchangeRate: $customExchangeRate)
-            }
-            Divider()
+        return GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    HStack {
+                        Spacer()
+                        DarkModeToggleButton(isDarkMode: $isDarkMode)
+                    }
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
+                    
+                    AmountInputView(euroAmount: $euroAmount)
+                    CurrencyPickerView(currencies: currencies, selectedCurrencyIndex: $selectedCurrencyIndex)
+                    ExchangeRateInputView(customExchangeRate: $customExchangeRate)
+                    HStack {
+                        TotalAmountView(convertedAmount: convertedAmount, selectedCurrency: currencies[selectedCurrencyIndex])
+                        ResetButton(euroAmount: $euroAmount, customExchangeRate: $customExchangeRate)
+                    }
+                    Divider()
+                        .padding()
+                    
+                    ExchangeRatePickerView(currencies: currencies, selectedRateCurrencyIndex: $selectedRateCurrencyIndex)
+                    
+                    AutomaticExchangeRatesView(automaticExchangeRates: automaticExchangeRates, selectedCurrency: currencies[selectedRateCurrencyIndex])
+                    
+                    Text("© 2023 Öncü Can. All rights reserved.")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                }
                 .padding()
-            
-            ExchangeRatePickerView(currencies: currencies, selectedRateCurrencyIndex: $selectedRateCurrencyIndex)
-            
-            AutomaticExchangeRatesView(automaticExchangeRates: automaticExchangeRates, selectedCurrency: currencies[selectedRateCurrencyIndex])
-            
-            Text("© 2023 Öncü Can. All rights reserved.")
-                .font(.footnote)
-                .foregroundColor(.gray)
+                .frame(minHeight: geometry.size.height)
+                .padding(.bottom, keyboard.currentHeight) // Klavye yüksekliğine göre alttan padding ekle
+                .animation(.easeOut(duration: 0.16)) // Görsel geçiş ekle
+            }
         }
-        .padding()
         .onAppear {
             fetchExchangeRates()
         }
@@ -66,6 +92,8 @@ struct CurrencyConvertView: View {
         }
     }
 }
+
+
 
 
 
