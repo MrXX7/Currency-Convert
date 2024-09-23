c8014c2fe8b4936db6cc0203f398e4f4b49e1090
Oncu <oohancan@gmail.com>
2024-09-23 22:19:58 +0300

    Prevent the "Öncü Can" footer from moving when the "Convert All" button is clicked. Adjusted layout to ensure only the exchange rate section transitions on button press.

--- Currency Convert/CurrencyConvertView.swift
+++ Currency Convert/CurrencyConvertView.swift
@@ -35,22 +35,11 @@ struct CurrencyConvertView: View {
     @State private var exchangeRates: [String: Double] = [:]
     @State private var selectedRateCurrencyIndex = 0
     @StateObject private var keyboard = KeyboardResponder()
-    
     @State private var showAllConversions = false
-
+    
     let currencies = ["EUR", "USD", "GBP", "TRY", "CAD", "CHF", "SAR", "AUD", "CNY"]
     let flags: [String: String] = ["EUR": "🇪🇺", "USD": "🇺🇸", "GBP": "🇬🇧", "TRY": "🇹🇷", "CAD": "🇨🇦", "CHF": "🇨🇭", "SAR": "🇸🇦", "AUD": "🇦🇺", "CNY": "🇨🇳"]
-    
     let quickAmounts: [String] = ["5", "10", "25", "50", "100", "200", "500", "1000"]
-    
-    
-    private func formatAmount(_ amount: String) -> String {
-        if let doubleValue = Double(amount), doubleValue.truncatingRemainder(dividingBy: 1) == 0 {
-            return String(format: "%.0f EUR", doubleValue)
-        } else {
-            return "\(amount) EUR"
-        }
-    }
 
     var body: some View {
         let convertedAmount = AmountConverter.convertAmount(euroAmount: euroAmount, customExchangeRate: customExchangeRate, selectedCurrencyIndex: selectedCurrencyIndex, exchangeRates: exchangeRates, currencies: currencies)
@@ -60,86 +49,109 @@ struct CurrencyConvertView: View {
         let allCurrencyConversions = convertAmountForAllCurrencies(baseAmount: euroAmount, baseCurrency: currencies[selectedCurrencyIndex])
 
         return GeometryReader { geometry in
-            ScrollView {
+            VStack {
+                // Üst kısımdaki kontroller sabit kalsın, sadece alt kısım değişsin
                 VStack(spacing: 5) {
+                    // Dark Mode Toggle
                     HStack {
                         Spacer()
                         DarkModeToggleButton(isDarkMode: $isDarkMode)
                     }
-                    
-                    .preferredColorScheme(isDarkMode ? .dark : .light)
                     .padding(.horizontal, 10)
+                    .preferredColorScheme(isDarkMode ? .dark : .light)
                     
+                    // Currency Selection Header
                     Text("Choose Your Currency")
-                        .font(.system(size: geometry.size.width > 320 ? 16 : 14)) // iPhone SE için daha küçük font
-
-                                .foregroundColor(.gray)
+                        .font(.system(size: geometry.size.width > 320 ? 16 : 14)) // Adjust font for small devices
+                        .foregroundColor(.gray)
+                    
+                    // Currency Picker
                     ExchangeRatePickerView(currencies: currencies, selectedRateCurrencyIndex: $selectedRateCurrencyIndex)
                         .frame(maxWidth: .infinity)
+                    
+                    // Quick Amount Picker
                     QuickAmountPickerView(quickAmounts: quickAmounts, euroAmount: $euroAmount)
                         .frame(maxWidth: .infinity)
+                    
+                    // Amount Input Field
                     AmountInputView(euroAmount: $euroAmount)
                         .frame(maxWidth: .infinity)
                     
+                    // Conversion Header
                     Text("Choose Currency to Convert To")
                         .font(.system(size: geometry.size.width > 320 ? 16 : 14))
                         .foregroundColor(.gray)
-                        .font(.system(size: 14, weight: .medium, design: .default))
-                        
+                    
+                    // Currency Picker for Conversion
                     CurrencyPickerView(currencies: currencies, selectedCurrencyIndex: $selectedCurrencyIndex)
                         .frame(maxWidth: .infinity)
+                    
+                    // "Convert All" Button
                     Button(action: {
                         showAllConversions.toggle()
                     }) {
                         HStack {
-                                Image(systemName: "arrow.2.circlepath")
-                                Text("Convert All")
+                            Image(systemName: "arrow.2.circlepath")
+                            Text("Convert All")
                                 .font(.footnote)
-                            }
-                        
-                    }.frame(maxWidth: .infinity)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .disabled(euroAmount.isEmpty || euroAmount == "0") // Disable button when input is empty
+                    
+                    // Custom Exchange Rate Input
                     ExchangeRateInputView(customExchangeRate: $customExchangeRate)
                     
-                    Divider() // Add divider here
+                    // Divider
+                    Divider()
                     
+                    // Total Amount and Reset Button
                     HStack {
                         TotalAmountView(convertedAmount: convertedAmount, selectedCurrency: currencies[selectedCurrencyIndex])
-                        Spacer() // Çok büyük boşluk bırakmaması için ekran boyutuna göre ayarlanabilir
+                        Spacer()
                         ResetButton(euroAmount: $euroAmount,
-                                        customExchangeRate: $customExchangeRate,
-                                        selectedCurrencyIndex: $selectedCurrencyIndex,
-                                        selectedRateCurrencyIndex: $selectedRateCurrencyIndex,
-                                        showAllConversions: $showAllConversions) // showAllConversions'ı da binding olarak veriyoruz.
-                        }
-                    .frame(maxWidth: .infinity) // Ekrana göre genişliği ayarla
-
+                                    customExchangeRate: $customExchangeRate,
+                                    selectedCurrencyIndex: $selectedCurrencyIndex,
+                                    selectedRateCurrencyIndex: $selectedRateCurrencyIndex,
+                                    showAllConversions: $showAllConversions)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                
+                // Alt kısımda sadece sonuçlar değişsin
+                VStack {
                     if showAllConversions {
                         VStack(alignment: .leading, spacing: 2) {
                             ForEach(allCurrencyConversions, id: \.self) { result in
                                 Text(result)
                                     .font(.subheadline)
-                                    .padding(.leading, -70)
+                                    .padding(.leading, 10)
                             }
                         }
-                        .padding(.leading, -90)
-                        .transition(.slide)
+                        .transition(.opacity)
                         .animation(.easeInOut(duration: 0.8))
                     } else {
                         AutomaticExchangeRatesView(automaticExchangeRates: automaticExchangeRates, selectedCurrency: currencies[selectedRateCurrencyIndex])
                             .transition(.slide)
                             .animation(.easeInOut)
                     }
-
-                    Text("© 2023 Öncü Can. All rights reserved.")
-                        .font(.footnote)
-                        .foregroundColor(.gray)
                 }
-                .padding()
                 .frame(maxWidth: .infinity)
-                    .frame(minHeight: geometry.size.height)
-                    .padding(.bottom, keyboard.currentHeight)
-                    .animation(.easeOut(duration: 0.6))
+                
+                Spacer()
+
+                // Footer sabit kalsın
+                Text("© 2023 Öncü Can. All rights reserved.")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 10)
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
+            .frame(minHeight: geometry.size.height) // Ensures full height of the screen
+            .padding(.bottom, keyboard.currentHeight) // Adjust for keyboard height
+            .animation(.easeOut(duration: 0.6))
         }
         .onAppear {
             fetchExchangeRates()
@@ -168,8 +180,9 @@ struct CurrencyConvertView: View {
         }
         return results
     }
-
 }
+
+
 
 
 
