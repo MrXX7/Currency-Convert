80abb3a80d80b63ec1c5ae19f04ce36644c17509
Oncu <oohancan@gmail.com>
2025-02-10 19:15:41 +0300

    Update AutomaticExchangeRatesView grid layout
    - Added horizontal padding to grid items
    - Ensured consistent spacing on left and right sides

--- Currency Convert/VIEW/AutomaticExchangeRatesView.swift
+++ Currency Convert/VIEW/AutomaticExchangeRatesView.swift
@@ -8,35 +8,35 @@
 import SwiftUI
 
 struct AutomaticExchangeRatesView: View {
-    @Environment(\.colorScheme) var colorScheme
-    var automaticExchangeRates: [String]
-    var selectedCurrency: String
+   @Environment(\.colorScheme) var colorScheme
+   var automaticExchangeRates: [String]
+   var selectedCurrency: String
 
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading, spacing: 2) {
-                Text("\(selectedCurrency) Exchange Rates")
-                    .font(.headline)
-                    .foregroundColor(colorScheme == .dark ? .white : .black)
-                    .padding(.bottom, 10)
-                
-                ForEach(automaticExchangeRates, id: \.self) { rate in
-                    Text(rate)
-                        .font(.subheadline)
-                        .foregroundColor(colorScheme == .dark ? .gray : .black)
-                        .padding(.bottom, 2)
-                }
-                
-                Spacer()
-            }
-            .padding(.leading, 10)
-                
-            Spacer()
-        }
-        .onTapGesture {
-            UIApplication.shared.windows.first?.endEditing(true)
-        }
-    }
+   var body: some View {
+       VStack {
+           Text("\(selectedCurrency) Exchange Rates")
+               .font(.headline)
+               .foregroundColor(colorScheme == .dark ? .white : .black)
+               .padding(.bottom, 10)
+           
+           LazyVGrid(columns: [
+               GridItem(.flexible(), spacing: 10),
+               GridItem(.flexible(), spacing: 10)
+           ], spacing: 8) {
+               ForEach(automaticExchangeRates, id: \.self) { rate in
+                   Text(rate)
+                       .font(.subheadline)
+                       .foregroundColor(colorScheme == .dark ? .gray : .black)
+                       .frame(maxWidth: .infinity, alignment: .leading)
+                       .padding(.horizontal, 5)
+               }
+           }
+           .padding(.horizontal, 10)
+       }
+       .onTapGesture {
+           UIApplication.shared.windows.first?.endEditing(true)
+       }
+   }
 }
 
 
